85556d74829437f55bcf29bb7bfaec9cc5e4a155
neojarvis <90308092+neojarvis@users.noreply.github.com>
2021-09-29 20:17:55 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and kill your self
         </a>
       </header>
     </div>
